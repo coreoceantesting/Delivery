67e971e34a05f7aeebcedda446addadaf070e29f
coreoceantesting <coreoceantesting@gmail.com>
2024-09-26 15:06:41 +0530

    changes

--- app.php
+++ app.php
@@ -1,9 +1,8 @@
 <?php
-
-echo "This is an new Project";
-echo "This is an new Capital";
-echo "This is an new Current";
-echo "This is an new Large";
-echo "This is an new Main";
-
+echo "This is an new project";
+echo "This is an new project";
+echo "This is an new project";
+echo "This is an new project";
+echo "This is an new project";
+echo "This is an new project";
 ?>
